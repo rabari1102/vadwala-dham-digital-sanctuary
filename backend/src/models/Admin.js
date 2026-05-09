4f7d6dc3b5274daa3c193f04ddb60e6615ea8c60
Pragnesh Kalotara <pragnesh.k@applocum.org>
2026-05-09 12:34:15 +0530

    chore: remove unused file configurations and associated logic

--- backend/src/models/Admin.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const adminSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true },
-}, { timestamps: true });
-
-module.exports = mongoose.model('Admin', adminSchema);
